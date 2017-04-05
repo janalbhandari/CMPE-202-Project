e1568806d2c02b290940d546376d2cabd267be62
janalbhandari <janalbhandari@gmail.com>
2017-04-05 16:21:55 -0700

    Update umlparser.java

--- umlparser.java
+++ umlparser.java
@@ -25,23 +25,8 @@ import static com.github.javaparser.ParseStart.*;
 import static com.github.javaparser.Providers.*;
 import static com.github.javaparser.utils.Utils.assertNotNull;
 
- private final CommentsInserter commentsInserter;
-    private final ParserConfiguration configuration;
-
-    private ASTParser aParser = null;
-
-    /**
-     * Instantiate the parser with default configuration. Note that parsing can also be done with the static methods on
-     * this class.
-     * Creating an instance will reduce setup time between parsing files.
-     */
-    public JavaParser() {
-        this(new ParserConfiguration());
-    }
-
- public JavaParser(ParserConfiguration configuration) {
-        this.configuration = configuration;
-        commentsInserter = new CommentsInserter(configuration);
-    }
-
-}
+ public class umlparser {
+		
+  inputfilepath = "/Users//Users/janalbhandari/Documents/CMPE-202/Project/umlparser/src";
+  
+ }
