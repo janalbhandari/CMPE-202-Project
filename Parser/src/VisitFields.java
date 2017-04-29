209e5d7b59f0269e00669e36c300b6d95c69765d
janalbhandari <janalbhandari@gmail.com>
2017-04-29 00:31:14 -0700

    Update VisitFields.java

--- Parser/src/VisitFields.java
+++ Parser/src/VisitFields.java
@@ -10,33 +10,49 @@ public class VisitFields extends VoidVisitorAdapter<Void>{
 	public void visit(FieldDeclaration n, Void arg) {
 		// TODO Auto-generated method stub
 		
-		ClassOrInterfaceDeclaration c = new ClassOrInterfaceDeclaration();
 		
 		
 		Parser p = new Parser();
-		
 		
 		if(n.getModifiers() == ModifierSet.PUBLIC){
 			
 			p.umlString += ("+" + n.getVariables().toString().replaceAll("\\[|\\]", "") + " : " + n.getType() + "\n");
 			
+				
+			
 		}
 		else if(n.getModifiers() == ModifierSet.PRIVATE)
 		{
 			if(!n.getVariables().toString().contains("="))
-			p.umlString += ("-" + n.getVariables().toString().replaceAll("\\[|\\]", "") + " : " + n.getType() + "\n");
+			{
+				if(n.getType().toString().contains("Collection")){
+					p.umlString += "";
+				}
+				else
+				{
+					p.umlString += ("-" + n.getVariables().toString().replaceAll("\\[|\\]", "") + " : " + n.getType() + "\n");
+				}
+			}
+			
+			//System.out.println(n.getType() + "   belongs to   " + p.cName);
+			
+			
+			/*if(n.getType().toString().contains("Collection")){
+				
+				String otherClass = n.getType().toString().substring(11, n.getType().toString().length()-1);
+				
+				
+					p.umlString +=p.cName + " ..> " + otherClass;
+				
+				}*/
 
 		}
-		else if(n.getModifiers() == ModifierSet.PROTECTED)
+		/*else if(n.getModifiers() == ModifierSet.PROTECTED)
 		{
 			
 			p.umlString += ("#" + n.getVariables().toString().replaceAll("\\[|\\]", "") + " : " + n.getType() + "\n");
-		}
-		
-		
-		
-		
-		
+		}*/
+	
 		super.visit(n, arg);
 	}
 	
